5cf26a8fbd23b6f010f4cc2f47e5cecf29bcd192
Ana-Maria Pintilie <ppe.anamaria@gmail.com>
2019-04-17 10:00:03 +0300

    Update extend_zabbix.sh

--- extend_zabbix.sh
+++ extend_zabbix.sh
@@ -9,10 +9,10 @@ for ((i=$START;i<=$END;i++)); do
     UserParameter=services.discovery,/etc/zabbix/external_scripts/machine_discover_services.sh
 
     #Get uptime of the service
-    UserParameter=machine_uptime_service[*],/etc/zabbix/external_scripts/machine_uptime_service.sh "$1"
+    UserParameter=machine_uptime_service[*],/etc/zabbix/external_scripts/machine_uptime_service.sh \$1
 
     #Get the status of the service
-    UserParameter=machine_status_service[*],systemctl is-active "$1"
+    UserParameter=machine_status_service[*],systemctl is-active \$1
     TEXT'
     echo "Restarting agent on machine $i"
     juju run --machine $i 'sudo systemctl restart zabbix-agent.service'
